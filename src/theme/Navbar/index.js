37d0d390c927142c746e3179d747bfbd761ad586
Buster Styren <buster.styren@gmail.com>
2022-08-22 11:36:07 +0200

    fix: margins on sign-in button on safari (due to flex+gap being broken on older versions)

--- src/theme/Navbar/index.js
+++ src/theme/Navbar/index.js
@@ -12,12 +12,12 @@ import ColorModeToggle from '@theme/ColorModeToggle';
 import {
   useHistoryPopHandler,
   useHideableNavbar,
+  useLockBodyScroll,
 } from '@docusaurus/theme-common/internal';
 import {
   useThemeConfig,
   useMobileSecondaryMenuRenderer,
   usePrevious,
-  useLockBodyScroll,
   useWindowSize,
   useColorMode,
 } from '@docusaurus/theme-common';
@@ -308,14 +308,18 @@ export default function Navbar() {
           <a
             target="_blank"
             href="https://app.symbiosis.host"
-            className="ml-2 text-sm shadow rounded bg-[color:var(--ifm-menu-color)] px-2.5 py-1.5 text-white font-semibold opacity-75 focus:opacity-100 focus:outline-none hover:opacity-100 hover:text-white hover:no-underline flex items-center gap-1"
+            className="ml-2 text-sm shadow rounded bg-[color:var(--ifm-menu-color)] px-2.5 py-1.5 text-white font-semibold opacity-75 focus:opacity-100 focus:outline-none hover:opacity-100 hover:text-white hover:no-underline flex items-center"
             aria-label="Twitter"
             rel="noreferrer"
           >
-            Sign-in <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-            width="12" height="12"
-          viewBox="0 0 24 24"
-    style={{fill:"#fff"}}><path d="M 5 3 C 3.9069372 3 3 3.9069372 3 5 L 3 19 C 3 20.093063 3.9069372 21 5 21 L 19 21 C 20.093063 21 21 20.093063 21 19 L 21 12 L 19 12 L 19 19 L 5 19 L 5 5 L 12 5 L 12 3 L 5 3 z M 14 3 L 14 5 L 17.585938 5 L 8.2929688 14.292969 L 9.7070312 15.707031 L 19 6.4140625 L 19 10 L 21 10 L 21 3 L 14 3 z"></path></svg>
+            Sign-in <svg
+              className="ml-1"
+              xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+              width="12" height="12"
+              viewBox="0 0 24 24"
+              style={{fill:"#fff"}}>
+              <path d="M 5 3 C 3.9069372 3 3 3.9069372 3 5 L 3 19 C 3 20.093063 3.9069372 21 5 21 L 19 21 C 20.093063 21 21 20.093063 21 19 L 21 12 L 19 12 L 19 19 L 5 19 L 5 5 L 12 5 L 12 3 L 5 3 z M 14 3 L 14 5 L 17.585938 5 L 8.2929688 14.292969 L 9.7070312 15.707031 L 19 6.4140625 L 19 10 L 21 10 L 21 3 L 14 3 z"></path>
+            </svg>
           </a>
           {rightItems.map((item, i) => (
             <NavbarItem {...item} key={i} />
